977b857644e480a0bb810c261e3d2eccb020150f
ityege <limou6233@gmail.com>
2022-08-28 17:45:09 +0800

    2022-08-28 17:44

---
+++ spark/src/main/java/cn/ityege/sparkcore/suanzi/Map.scala
@@ -0,0 +1,17 @@
+package cn.ityege.sparkcore.suanzi
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Map {
+  def main(args: Array[String]): Unit = {
+    val sparkConf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("测试程序")
+    val sparkContext: SparkContext = new SparkContext(sparkConf)
+    sparkContext.setLogLevel("ERROR")
+    val src: RDD[String] = sparkContext.parallelize(List("bit", "linc", "xwc", "fjg", "wc", "spark"), 3)
+    val map: RDD[Int] = src.map(word => word.length)
+    val zip: RDD[(String, Int)] = src.zip(map)
+    val collect: Array[(String, Int)] = zip.collect()
+    println(collect.mkString(","))
+  }
+}
